cfc86400d1cc2673923e954c470bea3d0702c1cc
Eduard Fedosevich <fedosewiched@gmail.com>
2022-01-19 21:03:13 +0300

    refactor: add numbers to hashtagCreate

--- src/main/hashtagCreate/hashtagCreateFunction.ts
+++ src/main/hashtagCreate/hashtagCreateFunction.ts
@@ -1,5 +1,5 @@
 export function hashtagCreate(text: string) {
-    let hashtags = text.match(/#[a-zа-я]+/gi)
+    let hashtags = text.match(/#[a-zа-я0-9]+/gi)
     if (hashtags === null) {
         hashtags = []
     }
